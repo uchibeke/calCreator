263c374b80006ecfe2d095d47644980eae60fc00
Uchi Uchibeke <u.i@usask.ca>
2017-01-15 01:23:45 -0500

    Save img as base64 in firebase

--- js/deal.js
+++ js/deal.js
@@ -19,28 +19,30 @@ function dealOps($rootScope, $scope, $http, $localStorage, $firebaseObject, $fir
 	var apiUrl = "https://calcreator-d490f.firebaseio.com/";
 
 	$scope.updateDeal = function(bizName, dealDescription, savings, dealStart, dealEnd) {
-		console.log(ss.deals);
 		var newDeal = {
 			"description" : dealDescription,
 			"savings" : savings,
 			"start" : dealStart,
 			"end" : dealEnd,
 			"phone" : ss.options.pNum,
-			"name" : ss.options.BizName
+			"name" : ss.options.BizName,
+			"logo" : ss.deals[BizName].logo
 		};
 		firebase.database().ref(bizName).set(newDeal);
 	};
+	
 	$scope.dealUrl = function() {
 		return hostingUrl + '%23!/d/' + encodeURIComponent(ss.options.BizName) + "/";
 	};
 
 	var hostingUrl = "http://calcreator.me/";
 
-	if (urlStrArr[urlStrArr.length - 2] === 'd') {
-		ss.options.bizNameFromUrl = urlStrArr[urlStrArr.length - 1];
-		ss.options.bizNameFromUrl = decodeURIComponent(ss.options.bizNameFromUrl);
+	ss.options.bizNameFromUrl = urlStrArr[urlStrArr.length - 1];
+	ss.options.bizNameFromUrl = decodeURIComponent(ss.options.bizNameFromUrl);
+	var nameToUse = ss.options.BizName;
+	if (urlStrArr[urlStrArr.length - 1] === 'design') {
 		if (myDeals.$loaded) {
-			$http.get(apiUrl + ss.options.bizNameFromUrl + ".json").then(function(response) {
+			$http.get(apiUrl + nameToUse + ".json").then(function(response) {
 				ss.loadedDeal = response.data;
 				// success callback
 			}, function(response) {
@@ -48,21 +50,49 @@ function dealOps($rootScope, $scope, $http, $localStorage, $firebaseObject, $fir
 				console.log(response.data);
 			});
 		}
-
-		ss.loadedDeal = ss.loadedDeal ? ss.loadedDeal : {};
+	} else {
+		nameToUse = urlStrArr[urlStrArr.length - 1];
+		if (myDeals.$loaded) {
+			$http.get(apiUrl + nameToUse + ".json").then(function(response) {
+				ss.loadedDeal = response.data;
+				// success callback
+			}, function(response) {
+				// failure callback
+				console.log(response.data);
+			});
+		}
 	}
 
-	function convertImageToBase64(img) {
-		console.log(img);
-		var canvas = document.createElement("canvas");
-		canvas.width = img.width;
-		canvas.height = img.height;
+	ss.loadedDeal = ss.loadedDeal ? ss.loadedDeal : {};
 
-		var ctx = canvas.getContext("2d");
-		ctx.drawImage(img, 0, 0);
+	$scope.uploadFiles = function(files, errFiles) {
+		$scope.files = files;
+		$scope.errFiles = errFiles;
+		console.log($scope.files);
+		console.log($scope.errFiles);
 
-		var dataURL = canvas.toDataURL("image/png");
+		window.setTimeout(function() {
+			var url = files[0]["$ngfBlobUrl"];
+			console.log(url);
 
-		return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-	}
+			var img = new Image();
+
+			img.setAttribute('crossOrigin', 'anonymous');
+
+			img.onload = function() {
+				var canvas = document.createElement("canvas");
+				canvas.width = this.width;
+				canvas.height = this.height;
+
+				var ctx = canvas.getContext("2d");
+				ctx.drawImage(this, 0, 0);
+
+				var dataURL = canvas.toDataURL("image/png");
+				var cleaned = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+				firebase.database().ref(ss.options.BizName + "/logo").set(cleaned);
+			};
+			img.src = url;
+		}, 1000);
+	};
+
 }
