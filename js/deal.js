8892df7fe905f1be61b801c58d8e01373552eafc
Uchi Uchibeke <u.i@usask.ca>
2017-01-16 00:24:14 -0500

    Fix bug

--- js/deal.js
+++ js/deal.js
@@ -26,7 +26,8 @@ function dealOps($rootScope, $scope, $http, $localStorage, $firebaseObject, $fir
 			"end" : dealEnd,
 			"phone" : ss.options.pNum,
 			"name" : ss.options.BizName,
-			"logo" : $scope.loadedDeal.logo ? $scope.loadedDeal.logo : "-"
+			"logo" : $scope.loadedDeal.logo ? $scope.loadedDeal.logo : "-",
+			"gal" : $scope.loadedDeal.gal
 		};
 		firebase.database().ref(ss.options.BizName).set(newDeal);
 	};
